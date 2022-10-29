bffd54de7820ea6e27bb80da9c672e947c7cf70a
Jeremy Banka <hello@jeremybanka.com>
2022-10-29 15:21:32 -0700

    ♻️ 🚧 reorganize around narrative/beat abstraction

--- hearts.lua
+++ hearts.lua
@@ -52,21 +52,15 @@ local function theirTurn(game, player)
   game:endTurn()
 end
 
----your turn
+---@class Beat : table
+---@field desc (fun(game: gamelib, player: player): string) | string
+---@field prompt (fun(game: gamelib, player: player): nil) | string | nil
+
+---prompt the player to play a card
 ---@param game gamelib
 ---@param player player
 ---@return nil
-local function yourTurn(game, player)
-
-  -- section 1
-  printHud(game)
-  speech.say("It's your turn!")
-  pressReturnTo("choose a card to play")
-
-  -- section 2
-  printHud(game)
-  print("* It's your turn!")
-  speech.say('Enter a number to play a card: ')
+local function promptCard(game, player)
   local playableCards = game:getPlayableCards(player.name)
   local playableCardNames = util.map(playableCards, cards.name)
   printer.list(playableCardNames, { indentFirstLine = 2, })
@@ -75,15 +69,89 @@ local function yourTurn(game, player)
   local cardIdx = io.read('*n')
   local chosenCard = playableCards[cardIdx] or playableCards[1]
   game:playCard(player.name, chosenCard)
-  _ = io.read() -- for some reason the program needs but ignores this line
+  _ = io.read() -- for some reason the program needs but also ignores this line
+end
+
+---describe card the player played
+---@param game gamelib
+---@param player player
+---@return string
+local function describeCardPlayed(game, player)
+  local lastPlayedCard = game:getCardPlayedThisTurn()
+  if lastPlayedCard then
+    local lastPlayedCardName = cards.name(lastPlayedCard)
+    return player.name .. " played " .. lastPlayedCardName .. "!"
+  else
+    return "But nobody played a card..."
+  end
+end
+
+---your turn
+---@param game gamelib
+---@param player player
+---@return nil
+local function yourTurn(game, player)
+  ---@type Beat[]
+  local narrative = {
+    {
+      desc = "It's your turn!",
+      prompt = "choose a card to play"
+    },
+    {
+      desc = 'Enter a number to play a card:',
+      prompt = promptCard
+    },
+    {
+      desc = describeCardPlayed,
+    },
+  }
+
+  -- section 1
+  printHud(game)
+  local desc1 = narrative[1].desc
+  if type(desc1) == 'function' then
+    desc1 = desc1(game, player)
+  end
+  speech.say(desc1)
+  local prompt = narrative[1].prompt
+  if type(prompt) == 'function' then
+    prompt(game, player)
+  else
+    pressReturnTo(prompt)
+  end
+
+  -- section 2
+  printHud(game)
+  local desc2 = narrative[2].desc
+  if type(desc2) == 'function' then
+    desc2 = desc2(game, player)
+  end
+  print("* " .. desc1)
+  speech.say(desc2)
+  local prompt = narrative[2].prompt
+  if type(prompt) == 'function' then
+    prompt(game, player)
+  else
+    pressReturnTo(prompt)
+  end
 
   -- section 3
   printHud(game)
-  print("* It's your turn!")
-  speech.say("You played " .. cards.name(chosenCard) .. ".")
-  pressReturnTo()
-  game:endTurn()
+  local desc3 = narrative[3].desc
+  if type(desc3) == 'function' then
+    desc3 = desc3(game, player)
+  end
+  print("* " .. desc1)
+  print("* " .. desc2)
+  speech.say(desc3)
+  local prompt = narrative[3].prompt
+  if type(prompt) == 'function' then
+    prompt(game, player)
+  else
+    pressReturnTo(prompt)
+  end
 
+  game:endTurn()
 end
 
 local function playGame()
