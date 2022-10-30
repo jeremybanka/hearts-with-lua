ec6a5a9fe655c4922fec0d7bd52c53d898aafe2d
Jeremy Banka <hello@jeremybanka.com>
2022-10-30 14:06:52 -0700

    ✨ add strategy module for Ralsei

--- lib/SECRET/strategy.lua
+++ lib/SECRET/strategy.lua
@@ -3,10 +3,7 @@ local cards = require "lib.cards"
 ---@class StrategyLib : table
 local strategylib = {}
 
----Kris remembers all cards played, predicts the end of the round for each card in their hand
----Ralsei always plays the HIGHEST CARD he can and avoids playing hearts
-
----play the first card that's allowed
+---no strategy: play the first card that's allowed
 ---@param game gamelib
 ---@param playerName string
 ---@return string
@@ -16,6 +13,7 @@ function strategylib.none(game, playerName)
 end
 
 ---Susie always plays the LOWEST HEART she Can, or the HIGHEST NON-HEART she can
+---she likes to brag about dumping points and playing face cards
 ---@param game gamelib
 ---@param playerName string
 ---@return string
@@ -34,7 +32,7 @@ function strategylib.Susie(game, playerName)
       game:interruptNarrative({ {
         speaker = "Susie",
         description = "Heh. Oops."
-      } })
+      } }, 2)
     end
     if (cards.getNumericRank(card) > 10) then
       game:interruptNarrative({ {
@@ -46,11 +44,72 @@ function strategylib.Susie(game, playerName)
   end
 end
 
----assess threat of points
+---Ralsei always plays the HIGHEST CARD he can and avoids playing hearts
+---Importantly, he considers who is likely to take the trick and says sorry
+---@param game gamelib
+---@param playerName string
+---@return string
+function strategylib.Ralsei(game, playerName)
+  local trickIsEmpty = #util.entries(game.trick) == 0
+  local taker, takersCard = game:getPlayer(playerName), nil
+  if not trickIsEmpty then
+    taker, takersCard = game:getTrickTaker()
+  end
+  local trickIsPainted = game:getTrickPoints() > 0
+  local playableCards = game:getPlayableCards(playerName)
+  local niceCards = cards.getHighestRank(cards.filterOutSuit(playableCards, "H"))
+  local isBeingNice = #niceCards ~= 0
+  local chosenCard = niceCards[1] or cards.getHighestRank(playableCards)[1]
+  local isRalseiGonnaWin = game:mayWinTrick(chosenCard)
+  local mayWin = isRalseiGonnaWin == "maybe"
+  local willWin = isRalseiGonnaWin == "yes"
+  local wontWin = isRalseiGonnaWin == "no"
+  if isBeingNice then
+    if takersCard ~= nil then
+      if mayWin or willWin then
+        game:interruptNarrative({ {
+          speaker = "Ralsei",
+          description = "Sorry, " .. taker.name .. "! Looks like I'm gonna take this one!"
+        } })
+        if trickIsPainted then
+          game:interruptNarrative({ {
+            speaker = playerName,
+            description = "I'm not worried about getting points. It's just fun to play!"
+          } })
+        end
+      end
+    end
+    if taker.name == "Susie" and wontWin then
+      game:interruptNarrative({ {
+        speaker = "Ralsei",
+        description = "Nice one, Susie! I'll get you next time!"
+      } })
+    end
+    return cards.getHighestRank(niceCards)[1]
+  else
+    if trickIsEmpty then
+      game:interruptNarrative({ {
+        speaker = playerName,
+        description = "Looks like I've got no choice..."
+      }, {
+        speaker = playerName,
+        description = "But don't worry, I'm sure this will end up being my trick!"
+      } })
+    elseif wontWin then
+      game:interruptNarrative({ {
+        speaker = playerName,
+        description = "Oh no... Sorry " .. taker.name .. "..."
+      } })
+    end
+    return cards.getHighestRank(playableCards)[1]
+  end
+end
+
+---fight or flight: noelle assesses the threat of points
 ---@param game gamelib
 ---@param playerName string
 ---@return integer
-function strategylib.assessThreat(game, playerName)
+function strategylib.NoelleAssessThreat(game, playerName)
   local threat = game:getTrickPoints()
   local howManyCardsUnseen = #game.players - #util.entries(game.trick) - 1
   if howManyCardsUnseen == 0 then
@@ -99,9 +158,7 @@ end
 ---@param playerName string
 ---@return string, NarrativeBeat?
 function strategylib.Noelle(game, playerName)
-  ---@type NarrativeBeat | nil
-  local interruption = nil
-  local threat = strategylib.assessThreat(game, playerName)
+  local threat = strategylib.NoelleAssessThreat(game, playerName)
   local player = game:getPlayer(playerName)
   local playableCards = game:getPlayableCards(playerName)
   local playerMustFollowSuit = util.some(
@@ -110,24 +167,47 @@ function strategylib.Noelle(game, playerName)
       return card:sub(-1) == game.leadingSuit
     end
   )
-  -- print(threat)
-  -- util.log(playableCards)
+  print(threat)
+  util.log(playableCards)
   local lowestCards = cards.getLowestRank(playableCards)
   local highestCards = cards.getHighestRank(playableCards)
 
   if threat >= 1 then
-    if playerMustFollowSuit then
+    local chosenCard = lowestCards[1]
+    if game:mayWinTrick(chosenCard) == "maybe" then
       -- print("lowest:", lowestCards[1])
       game:interruptNarrative({ {
         speaker = playerName,
-        description = "I don't want to get points!",
+        description = "I hope this is low enough to get by...",
       } })
-      return lowestCards[1]
     end
+    return chosenCard
   end
+  local chosenCard = highestCards[1]
   if playerMustFollowSuit then
-    if highestCards[1] == 'QS' then
-      return highestCards[2] or cards.getSecondHighestRank(playableCards)[1]
+    if chosenCard == 'QS' then
+      local fallbackCard = cards.getSecondHighestRank(playableCards)[1]
+      if fallbackCard == nil then
+        if game:mayWinTrick('QS') == "maybe" then
+          game:interruptNarrative({ {
+            speaker = playerName,
+            description = "Eek! It looks like I only have one choice!",
+          }, {
+            speaker = playerName,
+            description = "This could be bad...",
+          } })
+          return highestCards[1]
+        end
+      else
+        game:interruptNarrative({ {
+          speaker = playerName,
+          description = "I'm gonna play--",
+        }, {
+          speaker = playerName,
+          description = "--oh, no. Not that one.",
+        } })
+        return fallbackCard
+      end
     end
     -- print("highest:", highestCards[1])
     game:interruptNarrative({ {
@@ -141,7 +221,13 @@ function strategylib.Noelle(game, playerName)
     description = "Time to get rid of my high card. I'll play " .. cards.name(highestCards[1]) .. "..."
   } })
   -- print("highest:", highestCards[1])
-  return highestCards[1]
+  local highestSafeCard = util.filter(
+    highestCards,
+    function(card)
+      return not util.contains({ 'QS', 'KS', 'AS' }, card) and card:sub(-1) ~= 'H'
+    end
+  )[1] or cards.getSecondHighestRank(playableCards)[1]
+  return highestSafeCard
 end
 
 return strategylib
