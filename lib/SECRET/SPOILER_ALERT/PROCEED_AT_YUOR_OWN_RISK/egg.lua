2f33a0f8f5c4cfb413f1882ab5ea4c371e29b6c5
Jeremy Banka <hello@jeremybanka.com>
2022-10-29 19:14:09 -0700

    🥚

--- lib/SECRET/SPOILER_ALERT/PROCEED_AT_YUOR_OWN_RISK/egg.lua
+++ lib/SECRET/SPOILER_ALERT/PROCEED_AT_YUOR_OWN_RISK/egg.lua
@@ -1,16 +1,42 @@
+local util = require "lib.util"
 ---@class EasterEggs
 local egg = {}
+
 
 ---@param game gamelib
 ---@return nil
 function egg.lancerPlayed(game)
-  game:interruptNarrative({
-    {
+  local youGotLancered = {
+    speaker = "Lancer",
+    description = "HO HO HO!! YOU JUST GOT LANCERED!",
+    instruction = "acknowledge that you got lancered.",
+  }
+  ---@type table<"Kris" | "Susie" | "Ralsei" | "Noelle", NarrativeBeat[]>
+  local responsesToLancer = {
+    Kris = { {
+      speaker = "Kris",
+      description = "...",
+    } },
+    Susie = { {
+      speaker = "Susie",
+      description = "Yeah! Get Lancered, nerds!",
+    } },
+    Ralsei = { {
+      speaker = "Ralsei",
+      description = "Oh no! I'm sorry!",
+    } },
+    Noelle = { {
+      speaker = "Noelle",
+      description = "Fahaha! Have we met?",
+    }, {
       speaker = "Lancer",
-      description = "HO HO HO!! YOU JUST GOT LANCERED!",
-      instruction = "acknowledge that you got lancered.",
-    },
-  }, 2)
+      description = "I'm Lancer, and I'm here to say, I'm gonna Lancer you today!",
+    } }
+  }
+  local player = game:getCurrentPlayer()
+  local response = responsesToLancer[player.name]
+  local interruptionWithResponse = util.concat({ youGotLancered }, response)
+  game:interruptNarrative(interruptionWithResponse, 2)
 end
 
 return egg
